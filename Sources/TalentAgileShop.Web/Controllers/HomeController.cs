1079ce4a6ac35ed97ee034dd80af6bc13fa17f47
michel.perfetti <miiitch@gmail.com>
2016-12-23 11:24:34 +0100

    Initial commit

---
+++ Sources/TalentAgileShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TalentAgileShop.Web.Controllers
+{
+    [RoutePrefix("")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("catalog")]
+        public ActionResult Catalog()
+        {
+            ViewBag.Message = "Product catalog";
+
+            return View();
+        }
+
+        [Route("cart")]
+        public ActionResult Cart()
+        {
+            ViewBag.Message = "Cart";
+
+            return View();
+        }
+    }
+}
